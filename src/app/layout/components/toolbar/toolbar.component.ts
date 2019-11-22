34f8101ae26fdd850e18ef9c7bbf6a27ec935aa9
Logan Born <lcborn4@gmail.com>
2019-11-22 08:29:09 -0500

    formatting toolbar with loggedIn

--- src/app/layout/components/toolbar/toolbar.component.ts
+++ src/app/layout/components/toolbar/toolbar.component.ts
@@ -1,23 +1,21 @@
-import { Component, OnDestroy, OnInit, ViewEncapsulation } from '@angular/core';
-import { Subject } from 'rxjs';
-import { takeUntil } from 'rxjs/operators';
-import { TranslateService } from '@ngx-translate/core';
-import * as _ from 'lodash';
+import { Component, OnDestroy, OnInit, ViewEncapsulation } from "@angular/core";
+import { Subject } from "rxjs";
+import { takeUntil } from "rxjs/operators";
+import { TranslateService } from "@ngx-translate/core";
+import * as _ from "lodash";
 
-import { FuseConfigService } from '@fuse/services/config.service';
-import { FuseSidebarService } from '@fuse/components/sidebar/sidebar.service';
+import { FuseConfigService } from "@fuse/services/config.service";
+import { FuseSidebarService } from "@fuse/components/sidebar/sidebar.service";
 
-import { navigation } from 'app/navigation/navigation';
+import { navigation } from "app/navigation/navigation";
 
 @Component({
-    selector     : 'toolbar',
-    templateUrl  : './toolbar.component.html',
-    styleUrls    : ['./toolbar.component.scss'],
+    selector: "toolbar",
+    templateUrl: "./toolbar.component.html",
+    styleUrls: ["./toolbar.component.scss"],
     encapsulation: ViewEncapsulation.None
 })
-
-export class ToolbarComponent implements OnInit, OnDestroy
-{
+export class ToolbarComponent implements OnInit, OnDestroy {
     horizontalNavbar: boolean;
     rightNavbar: boolean;
     hiddenNavbar: boolean;
@@ -25,6 +23,8 @@ export class ToolbarComponent implements OnInit, OnDestroy
     navigation: any;
     selectedLanguage: any;
     userStatusOptions: any[];
+
+    LoggedIn: boolean;
 
     // Private
     private _unsubscribeAll: Subject<any>;
@@ -40,47 +40,48 @@ export class ToolbarComponent implements OnInit, OnDestroy
         private _fuseConfigService: FuseConfigService,
         private _fuseSidebarService: FuseSidebarService,
         private _translateService: TranslateService
-    )
-    {
+    ) {
+        this.LoggedIn = false;
+
         // Set the defaults
         this.userStatusOptions = [
             {
-                title: 'Online',
-                icon : 'icon-checkbox-marked-circle',
-                color: '#4CAF50'
+                title: "Online",
+                icon: "icon-checkbox-marked-circle",
+                color: "#4CAF50"
             },
             {
-                title: 'Away',
-                icon : 'icon-clock',
-                color: '#FFC107'
+                title: "Away",
+                icon: "icon-clock",
+                color: "#FFC107"
             },
             {
-                title: 'Do not Disturb',
-                icon : 'icon-minus-circle',
-                color: '#F44336'
+                title: "Do not Disturb",
+                icon: "icon-minus-circle",
+                color: "#F44336"
             },
             {
-                title: 'Invisible',
-                icon : 'icon-checkbox-blank-circle-outline',
-                color: '#BDBDBD'
+                title: "Invisible",
+                icon: "icon-checkbox-blank-circle-outline",
+                color: "#BDBDBD"
             },
             {
-                title: 'Offline',
-                icon : 'icon-checkbox-blank-circle-outline',
-                color: '#616161'
+                title: "Offline",
+                icon: "icon-checkbox-blank-circle-outline",
+                color: "#616161"
             }
         ];
 
         this.languages = [
             {
-                id   : 'en',
-                title: 'English',
-                flag : 'us'
+                id: "en",
+                title: "English",
+                flag: "us"
             },
             {
-                id   : 'tr',
-                title: 'Turkish',
-                flag : 'tr'
+                id: "tr",
+                title: "Turkish",
+                flag: "tr"
             }
         ];
 
@@ -97,26 +98,27 @@ export class ToolbarComponent implements OnInit, OnDestroy
     /**
      * On init
      */
-    ngOnInit(): void
-    {
+    ngOnInit(): void {
         // Subscribe to the config changes
         this._fuseConfigService.config
             .pipe(takeUntil(this._unsubscribeAll))
-            .subscribe((settings) => {
-                this.horizontalNavbar = settings.layout.navbar.position === 'top';
-                this.rightNavbar = settings.layout.navbar.position === 'right';
+            .subscribe(settings => {
+                this.horizontalNavbar =
+                    settings.layout.navbar.position === "top";
+                this.rightNavbar = settings.layout.navbar.position === "right";
                 this.hiddenNavbar = settings.layout.navbar.hidden === true;
             });
 
         // Set the selected language from default languages
-        this.selectedLanguage = _.find(this.languages, {id: this._translateService.currentLang});
+        this.selectedLanguage = _.find(this.languages, {
+            id: this._translateService.currentLang
+        });
     }
 
     /**
      * On destroy
      */
-    ngOnDestroy(): void
-    {
+    ngOnDestroy(): void {
         // Unsubscribe from all subscriptions
         this._unsubscribeAll.next();
         this._unsubscribeAll.complete();
@@ -131,8 +133,7 @@ export class ToolbarComponent implements OnInit, OnDestroy
      *
      * @param key
      */
-    toggleSidebarOpen(key): void
-    {
+    toggleSidebarOpen(key): void {
         this._fuseSidebarService.getSidebar(key).toggleOpen();
     }
 
@@ -141,8 +142,7 @@ export class ToolbarComponent implements OnInit, OnDestroy
      *
      * @param value
      */
-    search(value): void
-    {
+    search(value): void {
         // Do your search here...
         console.log(value);
     }
@@ -152,12 +152,15 @@ export class ToolbarComponent implements OnInit, OnDestroy
      *
      * @param lang
      */
-    setLanguage(lang): void
-    {
+    setLanguage(lang): void {
         // Set the selected language for the toolbar
         this.selectedLanguage = lang;
 
         // Use the selected language for translations
         this._translateService.use(lang.id);
+    }
+
+    login(): void {
+        this.LoggedIn = true;
     }
 }
