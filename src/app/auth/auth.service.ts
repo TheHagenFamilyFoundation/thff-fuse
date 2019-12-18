ecb7a0673ad54fd28d720c77edc9e5905b6769a0
Logan Born <lcborn4@gmail.com>
2019-12-18 00:14:19 -0500

    added auth service

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,108 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { map } from 'rxjs/operators';
+
+import { Observable } from 'rxjs'
+
+import { environment } from '../../environments/environment';
+
+import { JwtHelperService } from '@auth0/angular-jwt';
+
+@Injectable({ providedIn: 'root' })
+export class AuthService {
+
+    API_URL: string;
+
+    jwtHelper = new JwtHelperService();
+
+    constructor(private http: HttpClient, private router: Router) {
+
+        if (!environment.production) {
+            this.API_URL = environment.API_URL;
+        }
+        else {
+            this.API_URL = this.getBackendURL();
+            console.log('this.API_URL', this.API_URL)
+        }
+
+        console.log('this.API_URL', this.API_URL)
+
+    }
+
+    login(data, csrf) {
+
+        console.log('LOGIN - data', data)
+
+        if (!environment.production) {
+            this.API_URL = environment.API_URL;
+        }
+        else {
+            this.API_URL = this.getBackendURL();
+
+            console.log('auth service - this.API_URL', this.API_URL)
+        }
+
+        console.log('login - this.API_URL', this.API_URL)
+
+        return this.http.put<any>(this.API_URL + `/login`, data)
+            .pipe(map(result => {
+
+                console.log('result', result)
+
+                //new 
+                //sends the result to the login component which thens add to localStorage
+
+                // old
+                // login successful if there's a jwt token in the response
+                // if (result && result.token) {
+                //     // store user details and jwt token in local storage to keep user logged in between page refreshes
+                //     localStorage.setItem('currentUser', JSON.stringify(result.user));
+                // }
+
+                return result;
+            }));
+    }
+
+    logout() {
+        console.log("logging out")
+
+        // Remove tokens and profile and update login status subject
+        localStorage.removeItem('token');
+        localStorage.removeItem('currentUser');
+
+        this.router.navigate(['/home']);
+
+    }
+
+    tokenGetter() {
+        return localStorage.getItem('token');
+    }
+
+    isExpired() {
+        return this.jwtHelper.isTokenExpired(this.tokenGetter());
+    }
+
+    initializeBackendURL(): Observable<any> {
+
+        if (environment.production == true) {
+
+            console.log('getting backend URL', window.location.origin + '/backend')
+
+            return this.http.get(window.location.origin + '/backend')
+
+        }
+
+    }
+
+    getBackendURL(): string {
+
+        return sessionStorage.getItem('backend_url');
+
+    }
+
+    clearBackendURL() {
+        sessionStorage.removeItem('backend_url');
+    }
+
+}
