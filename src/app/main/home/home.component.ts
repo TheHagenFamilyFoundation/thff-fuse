bfa7adea458d46537607a9934788439456bc1657
Logan Born <lcborn4@gmail.com>
2019-12-18 21:50:56 -0500

    added dthe images

--- src/app/main/home/home.component.ts
+++ src/app/main/home/home.component.ts
@@ -1,17 +1,22 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
-import { FuseTranslationLoaderService } from '@fuse/services/translation-loader.service';
+import { FuseTranslationLoaderService } from "@fuse/services/translation-loader.service";
 
-import { locale as english } from './i18n/en';
-import { locale as turkish } from './i18n/tr';
+import { locale as english } from "./i18n/en";
+import { locale as turkish } from "./i18n/tr";
 
 @Component({
-    selector   : 'home',
-    templateUrl: './home.component.html',
-    styleUrls  : ['./home.component.scss']
+    selector: "home",
+    templateUrl: "./home.component.html",
+    styleUrls: ["./home.component.scss"]
 })
-export class HomeComponent
-{
+export class HomeComponent {
+    /*
+Images
+*/
+    fullImagePath = "../assets/images/thfflogo1.png";
+    fullImagePath2 = "../assets/images/cv_busses1.JPG";
+
     /**
      * Constructor
      *
@@ -19,9 +24,7 @@ export class HomeComponent
      */
     constructor(
         private _fuseTranslationLoaderService: FuseTranslationLoaderService
-    )
-    // {}
-    {
+    ) {
         this._fuseTranslationLoaderService.loadTranslations(english, turkish);
     }
 }
