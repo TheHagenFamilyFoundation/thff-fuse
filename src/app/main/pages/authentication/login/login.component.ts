10d58609d31b84d298a3369864504de69315725b
Logan Born <lcborn4@gmail.com>
2019-12-22 22:31:55 -0500

    format login

--- src/app/main/pages/authentication/login/login.component.ts
+++ src/app/main/pages/authentication/login/login.component.ts
@@ -1,30 +1,31 @@
-import { Component, OnInit, ViewEncapsulation } from "@angular/core";
-import { Router } from "@angular/router";
-import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
-import { FuseConfigService } from "@fuse/services/config.service";
-import { fuseAnimations } from "@fuse/animations";
+import { FuseConfigService } from '@fuse/services/config.service';
+import { fuseAnimations } from '@fuse/animations';
 
-import { AuthService } from "../../../../auth/auth.service";
+import { AuthService } from '../../../../auth/auth.service';
 
-import { environment } from "../../../../../environments/environment";
+import { environment } from '../../../../../environments/environment';
 
 @Component({
-    selector: "login",
-    templateUrl: "./login.component.html",
-    styleUrls: ["./login.component.scss"],
-    encapsulation: ViewEncapsulation.None,
-    animations: fuseAnimations
+  selector: 'login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+  encapsulation: ViewEncapsulation.None,
+  animations: fuseAnimations,
 })
 export class LoginComponent implements OnInit {
     loginForm: FormGroup;
 
-    title = "LOGIN TO YOUR ACCOUNT"; //default
-    //when submitting changes to LOGGING IN...
+    title = 'LOGIN TO YOUR ACCOUNT'; // default
+    // when submitting changes to LOGGING IN...
 
     API_URL: string;
 
     ShowMessage = false;
+
     message: any;
 
     Login: any;
@@ -35,163 +36,163 @@ export class LoginComponent implements OnInit {
      * Constructor
      *
      * @param {FuseConfigService} _fuseConfigService
-     * @param {FormBuilder} _formBuilder
+     * @param {FormBuilder} formBuilder
      */
     constructor(
-        private _router: Router,
-        private _fuseConfigService: FuseConfigService,
-        private _formBuilder: FormBuilder,
-        private _authService: AuthService
+        private router: Router,
+        private fuseConfigService: FuseConfigService,
+        private formBuilder: FormBuilder,
+        private authService: AuthService,
     ) {
-        // Configure the layout
-        this._fuseConfigService.config = {
-            layout: {
-                navbar: {
-                    hidden: true
-                },
-                toolbar: {
-                    hidden: true
-                },
-                footer: {
-                    hidden: true
-                },
-                sidepanel: {
-                    hidden: true
-                }
-            }
-        };
+      // Configure the layout
+      this.fuseConfigService.config = {
+        layout: {
+          navbar: {
+            hidden: true,
+          },
+          toolbar: {
+            hidden: true,
+          },
+          footer: {
+            hidden: true,
+          },
+          sidepanel: {
+            hidden: true,
+          },
+        },
+      };
 
-        if (!environment.production) {
-            this.API_URL = environment.API_URL;
-        }
+      if (!environment.production) {
+        this.API_URL = environment.API_URL;
+      }
 
-        this.getBackendURL();
+      this.getBackendURL();
 
-        console.log("this.API_URL", this.API_URL);
+      console.log('this.API_URL', this.API_URL);
     }
 
-    // -----------------------------------------------------------------------------------------------------
+    // --------------------------------------------------------------------------------------------
     // @ Lifecycle hooks
-    // -----------------------------------------------------------------------------------------------------
+    // --------------------------------------------------------------------------------------------
 
     /**
      * On init
      */
     ngOnInit(): void {
-        this.loginForm = this._formBuilder.group({
-            email: ["", [Validators.required, Validators.email]],
-            password: ["", Validators.required]
-        });
+      this.loginForm = this.formBuilder.group({
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', Validators.required],
+      });
     }
 
     onSubmit(): void {
-        console.log("submitting");
-        console.log("loginForm", this.loginForm);
-        this.title = "LOGGING IN";
+      console.log('submitting');
+      console.log('loginForm', this.loginForm);
+      this.title = 'LOGGING IN';
 
-        this.Submitted = true;
+      this.Submitted = true;
 
-        let email = this.loginForm.value.email;
-        let password = this.loginForm.value.password;
+      const { email } = this.loginForm.value;
+      const { password } = this.loginForm.value;
 
-        console.log("email = " + email);
-        console.log("password = " + password);
+      console.log(`email = ${email}`);
+      console.log(`password = ${password}`);
 
-        // this.CSRF = this.getCSRFTokenService.getCSRF()
-        //   .subscribe(
-        //     (data) => {
+      // this.CSRF = this.getCSRFTokenService.getCSRF()
+      //   .subscribe(
+      //     (data) => {
 
-        // console.log('after CSRF', this.API_URL)
+      // console.log('after CSRF', this.API_URL)
 
-        // if (data._csrf) {
-        //   this.csrfToken = data._csrf;
-        // }
+      // if (data._csrf) {
+      //   this.csrfToken = data._csrf;
+      // }
 
-        //login url
-        let urlString = this.API_URL + "/login";
+      // login url
+      const urlString = `${this.API_URL}/login`;
 
-        let body = {
-            email: email,
-            password: password
-            // _csrf: this.csrfToken //|| undefined
-        };
+      const body = {
+        email,
+        password,
+        // _csrf: this.csrfToken //|| undefined
+      };
 
-        // if (this.csrfToken) {
-        //   this.body._csrf = this.csrfToken;
-        // }
+      // if (this.csrfToken) {
+      //   this.body._csrf = this.csrfToken;
+      // }
 
-        console.log("body", body);
-        console.log("urlString", urlString);
+      console.log('body', body);
+      console.log('urlString', urlString);
 
-        this.Login = this._authService
-            .login(body, null) //, this.csrfToken)
-            .subscribe(
-                data => {
-                    console.log("data", data);
-                    let user = data.user;
-                    console.log("user", user);
+      this.Login = this.authService
+        .login(body, null) // , this.csrfToken)
+        .subscribe(
+          (data) => {
+            console.log('data', data);
+            const { user } = data;
+            console.log('user', user);
 
-                    if (user) {
-                        console.log("this.results.user = ", user);
+            if (user) {
+              console.log('this.results.user = ', user);
 
-                        localStorage.setItem("token", data.token);
-                        localStorage.setItem(
-                            "currentUser",
-                            JSON.stringify(user)
-                        );
+              localStorage.setItem('token', data.token);
+              localStorage.setItem(
+                'currentUser',
+                JSON.stringify(user),
+              );
 
-                        console.log("token = " + localStorage.getItem("token"));
-                        console.log(
-                            "currentUser = " +
-                                localStorage.getItem("currentUser")
-                        );
+              console.log(`token = ${localStorage.getItem('token')}`);
+              console.log(
+                `currentUser = ${
+                  localStorage.getItem('currentUser')}`,
+              );
 
-                        this._router.navigate(["/pages/user"]);
-                    } else {
-                        this.message = data.message;
-                        this.ShowMessage = true;
-                        this.title = "LOGIN TO YOUR ACCOUNT";
-                    }
-                },
-                error => {
-                    console.log("error", error);
+              this.router.navigate(['/pages/user']);
+            } else {
+              this.message = data.message;
+              this.ShowMessage = true;
+              this.title = 'LOGIN TO YOUR ACCOUNT';
+            }
+          },
+          (error) => {
+            console.log('error', error);
 
-                    this.message = error.error.message;
+            this.message = error.error.message;
 
-                    console.log("message", this.message);
+            console.log('message', this.message);
 
-                    this.title = "LOGIN TO YOUR ACCOUNT";
-                    this.ShowMessage = true;
-                    this.Submitted = false;
-                }
-            );
+            this.title = 'LOGIN TO YOUR ACCOUNT';
+            this.ShowMessage = true;
+            this.Submitted = false;
+          },
+        );
 
-        //})
+      // })
     }
 
     getBackendURL() {
-        console.log("environment", environment);
-        if (environment.production) {
-            this.API_URL = environment.API_URL;
+      console.log('environment', environment);
+      if (environment.production) {
+        this.API_URL = environment.API_URL;
 
-            this._authService.initializeBackendURL().subscribe(backendUrl => {
-                console.log("backendUrl", backendUrl);
-                console.log("backendUrl", backendUrl.url);
+        this.authService.initializeBackendURL().subscribe((backendUrl) => {
+          console.log('backendUrl', backendUrl);
+          console.log('backendUrl', backendUrl.url);
 
-                if (backendUrl) {
-                    sessionStorage.setItem("backend_url", backendUrl.url);
-                } else {
-                    console.log(
-                        "Can´t find the backend URL, using a failover value"
-                    );
-                    sessionStorage.setItem(
-                        "backend_url",
-                        "https://failover-url.com"
-                    );
-                }
+          if (backendUrl) {
+            sessionStorage.setItem('backend_url', backendUrl.url);
+          } else {
+            console.log(
+              'Can´t find the backend URL, using a failover value',
+            );
+            sessionStorage.setItem(
+              'backend_url',
+              'https://failover-url.com',
+            );
+          }
 
-                this.API_URL = backendUrl.url || "http://localhost:1337";
-            });
-        }
+          this.API_URL = backendUrl.url || 'http://localhost:1337';
+        });
+      }
     }
 }
--- src/app/main/pages/authentication/login/login.module.ts
+++ src/app/main/pages/authentication/login/login.module.ts
@@ -1,36 +1,36 @@
-import { NgModule } from "@angular/core";
-import { RouterModule } from "@angular/router";
-import { MatButtonModule } from "@angular/material/button";
-import { MatCheckboxModule } from "@angular/material/checkbox";
-import { MatFormFieldModule } from "@angular/material/form-field";
-import { MatIconModule } from "@angular/material/icon";
-import { MatInputModule } from "@angular/material/input";
-import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
-import { FuseSharedModule } from "@fuse/shared.module";
+import { FuseSharedModule } from '@fuse/shared.module';
 
-import { LoginComponent } from "app/main/pages/authentication/login/login.component";
+import { LoginComponent } from 'app/main/pages/authentication/login/login.component';
 
 const routes = [
-    {
-        path: "auth/login",
-        component: LoginComponent
-    }
+  {
+    path: 'auth/login',
+    component: LoginComponent,
+  },
 ];
 
 @NgModule({
-    declarations: [LoginComponent],
-    imports: [
-        RouterModule.forChild(routes),
+  declarations: [LoginComponent],
+  imports: [
+    RouterModule.forChild(routes),
 
-        MatButtonModule,
-        MatCheckboxModule,
-        MatFormFieldModule,
-        MatIconModule,
-        MatInputModule,
-        MatProgressSpinnerModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatProgressSpinnerModule,
 
-        FuseSharedModule
-    ]
+    FuseSharedModule,
+  ],
 })
 export class LoginModule {}
