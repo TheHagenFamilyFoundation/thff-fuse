ad73f020429dbc5826d42cfddaa7714018c120f3
Logan Born <lcborn4@gmail.com>
2019-12-22 22:31:20 -0500

    format home component

--- src/app/main/home/home.component.ts
+++ src/app/main/home/home.component.ts
@@ -1,30 +1,31 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
-import { FuseTranslationLoaderService } from "@fuse/services/translation-loader.service";
+import { FuseTranslationLoaderService } from '@fuse/services/translation-loader.service';
 
-import { locale as english } from "./i18n/en";
-import { locale as turkish } from "./i18n/tr";
+import { locale as english } from './i18n/en';
+import { locale as turkish } from './i18n/tr';
 
 @Component({
-    selector: "home",
-    templateUrl: "./home.component.html",
-    styleUrls: ["./home.component.scss"]
+  selector: 'home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
     /*
 Images
 */
-    fullImagePath = "../assets/images/thfflogo1.png";
-    fullImagePath2 = "../assets/images/cv_busses1.JPG";
+    fullImagePath = '../assets/images/thfflogo1.png';
+
+    fullImagePath2 = '../assets/images/cv_busses1.JPG';
 
     /**
      * Constructor
      *
-     * @param {FuseTranslationLoaderService} _fuseTranslationLoaderService
+     * @param {FuseTranslationLoaderService} fuseTranslationLoaderService
      */
     constructor(
-        private _fuseTranslationLoaderService: FuseTranslationLoaderService
+        private fuseTranslationLoaderService: FuseTranslationLoaderService,
     ) {
-        this._fuseTranslationLoaderService.loadTranslations(english, turkish);
+      this.fuseTranslationLoaderService.loadTranslations(english, turkish);
     }
 }
