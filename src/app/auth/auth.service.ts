34c5220f6f695255de27ad116f4de308e1f91f4f
Logan <lcborn4@gmail.com>
2019-12-28 09:30:45 -0500

    lint on auth service

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { map } from 'rxjs/operators';
 
-import { Observable } from 'rxjs'
+import { Observable } from 'rxjs';
 
 import { environment } from '../../environments/environment';
 
@@ -11,98 +11,85 @@ import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
-
     API_URL: string;
 
     jwtHelper = new JwtHelperService();
 
     constructor(private http: HttpClient, private router: Router) {
+      if (!environment.production) {
+        this.API_URL = environment.API_URL;
+      } else {
+        this.API_URL = this.getBackendURL();
+        console.log('this.API_URL', this.API_URL);
+      }
 
-        if (!environment.production) {
-            this.API_URL = environment.API_URL;
-        }
-        else {
-            this.API_URL = this.getBackendURL();
-            console.log('this.API_URL', this.API_URL)
-        }
-
-        console.log('this.API_URL', this.API_URL)
-
+      console.log('this.API_URL', this.API_URL);
     }
 
-    login(data, csrf) {
+    // login(data,csrf)
+    login(data) {
+      console.log('LOGIN - data', data);
 
-        console.log('LOGIN - data', data)
+      if (!environment.production) {
+        this.API_URL = environment.API_URL;
+      } else {
+        this.API_URL = this.getBackendURL();
 
-        if (!environment.production) {
-            this.API_URL = environment.API_URL;
-        }
-        else {
-            this.API_URL = this.getBackendURL();
+        console.log('auth service - this.API_URL', this.API_URL);
+      }
 
-            console.log('auth service - this.API_URL', this.API_URL)
-        }
+      console.log('login - this.API_URL', this.API_URL);
 
-        console.log('login - this.API_URL', this.API_URL)
+      return this.http.put<any>(`${this.API_URL}/login`, data)
+        .pipe(map((result) => {
+          console.log('result', result);
 
-        return this.http.put<any>(this.API_URL + `/login`, data)
-            .pipe(map(result => {
+          // new
+          // sends the result to the login component which thens add to localStorage
 
-                console.log('result', result)
+          // old
+          // login successful if there's a jwt token in the response
+          // if (result && result.token) {
+          // store user details and jwt token in local storage to keep user logged in
+          // between page refreshes
+          //     localStorage.setItem('currentUser', JSON.stringify(result.user));
+          // }
 
-                //new 
-                //sends the result to the login component which thens add to localStorage
-
-                // old
-                // login successful if there's a jwt token in the response
-                // if (result && result.token) {
-                //     // store user details and jwt token in local storage to keep user logged in between page refreshes
-                //     localStorage.setItem('currentUser', JSON.stringify(result.user));
-                // }
-
-                return result;
-            }));
+          return result;
+        }));
     }
 
     logout() {
-        console.log("logging out")
+      console.log('logging out');
 
-        // Remove tokens and profile and update login status subject
-        localStorage.removeItem('token');
-        localStorage.removeItem('currentUser');
+      // Remove tokens and profile and update login status subject
+      localStorage.removeItem('token');
+      localStorage.removeItem('currentUser');
 
-        this.router.navigate(['/home']);
-
+      this.router.navigate(['/home']);
     }
 
     tokenGetter() {
-        return localStorage.getItem('token');
+      return localStorage.getItem('token');
     }
 
     isExpired() {
-        return this.jwtHelper.isTokenExpired(this.tokenGetter());
+      return this.jwtHelper.isTokenExpired(this.tokenGetter());
     }
 
     initializeBackendURL(): Observable<any> {
+      if (environment.production === true) {
+        console.log('getting backend URL', `${window.location.origin}/backend`);
 
-        if (environment.production == true) {
-
-            console.log('getting backend URL', window.location.origin + '/backend')
-
-            return this.http.get(window.location.origin + '/backend')
-
-        }
-
+        return this.http.get(`${window.location.origin}/backend`);
+      }
     }
 
     getBackendURL(): string {
-
-        return sessionStorage.getItem('backend_url');
-
+      return sessionStorage.getItem('backend_url');
     }
 
     clearBackendURL() {
-        sessionStorage.removeItem('backend_url');
+      sessionStorage.removeItem('backend_url');
     }
-
 }
