af074fa3980ac77cc75e28b87a6f4e30784a821c
Logan <lcborn4@gmail.com>
2019-12-30 20:36:51 -0500

    lint on layout

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -3,13 +3,12 @@ import { NgModule } from '@angular/core';
 import { VerticalLayout1Module } from 'app/layout/vertical/layout-1/layout-1.module';
 
 @NgModule({
-    imports: [
-        VerticalLayout1Module
-    ],
-    exports: [
-        VerticalLayout1Module
-    ]
+  imports: [
+    VerticalLayout1Module,
+  ],
+  exports: [
+    VerticalLayout1Module,
+  ],
 })
-export class LayoutModule
-{
+export class LayoutModule {
 }
