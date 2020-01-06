0571c41acc0e551ee6f7372aa0c091ebe4333b50
Logan Born <lcborn4@gmail.com>
2020-01-05 22:57:05 -0500

    lint on spec for grants-awarded-total

--- src/app/main/home/grants-awarded-total/grants-awarded-total.component.spec.ts
+++ src/app/main/home/grants-awarded-total/grants-awarded-total.component.spec.ts
@@ -8,9 +8,9 @@ describe('GrantsAwardedTotalComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ GrantsAwardedTotalComponent ]
+      declarations: [GrantsAwardedTotalComponent],
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
