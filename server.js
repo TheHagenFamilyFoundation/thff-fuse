d9f1f6df6e069fe2d830aadada272cfa24c034be
Logan Born <lcborn4@gmail.com>
2020-01-19 17:16:03 -0500

    added path

--- server.js
+++ server.js
@@ -4,18 +4,18 @@ const app = express();
 const path = require('path');
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static(__dirname + '/dist/'));
+app.use(express.static(`${__dirname}/dist/`));
 
 // Nice and done!
 app.get('/backend', (req, res) => {
-  res.send({ url: process.env.BE_API })
+  res.send({ url: process.env.BE_API });
 });
 
 
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(`${__dirname}/dist/index.html`));
 });
 
 // Start the app by listening on the default
