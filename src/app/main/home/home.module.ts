4f06e72f6d66e1fffe071bf3af621e609b7a96f0
Logan Born <lcborn4@gmail.com>
2020-01-30 00:24:17 -0500

    about and grantssince added

--- src/app/main/home/home.module.ts
+++ src/app/main/home/home.module.ts
@@ -11,9 +11,10 @@ import { SlideshowModule } from 'ng-simple-slideshow';
 import { GrantService } from '../../services/grants/grant.service';
 import { GrantApiService } from '../../services/grants/grant-api.service';
 
-
 import { HomeComponent } from './home.component';
 import { GrantsAwardedTotalComponent } from './grants-awarded-total/grants-awarded-total.component';
+import { GrantsSinceComponent } from './grants-since/grants-since.component';
+import { AboutComponent } from './about/about.component';
 
 const routes = [
   {
@@ -26,6 +27,8 @@ const routes = [
   declarations: [
     HomeComponent,
     GrantsAwardedTotalComponent,
+    GrantsSinceComponent,
+    AboutComponent,
   ],
   imports: [
     RouterModule.forChild(routes),
