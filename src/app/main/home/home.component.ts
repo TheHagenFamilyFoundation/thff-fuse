8f01ba1cdee0f791f72189311b8efd3735272009
Logan Born <lcborn4@gmail.com>
2020-02-02 12:40:59 -0500

    old home component services added, get the env variabel

--- src/app/main/home/home.component.ts
+++ src/app/main/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { environment } from '../../../environments/environment';
 
@@ -11,15 +11,35 @@ import { InterfaceImage } from './IImage';
 import { locale as english } from './i18n/en';
 import { locale as turkish } from './i18n/tr';
 
+import { GetUserService } from '../../services/user/get-user.service'; // used for getting organizations
+import { InOrgService } from '../../services/user/in-org.service';
+import { DirectorService } from '../../services/user/director.service';
+
 @Component({
   selector: 'home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  title = 'Home'
+
   env: any;
 
   API: any;
+
+  currentUser: any;
+
+  userName: string;
+
+  accessLevel: number; // debug
+
+  InOrganization: boolean;
+
+  inOrgCheck: boolean;
+
+  IsDirector: boolean;
+
+  organizations: any;
 
   /*
     Images
@@ -52,10 +72,133 @@ export class HomeComponent {
   */
   constructor(
     public authService: AuthService,
+    private inOrg: InOrgService,
+    private getUserService: GetUserService,
+    private directorService: DirectorService,
     private fuseTranslationLoaderService: FuseTranslationLoaderService,
   ) {
     this.fuseTranslationLoaderService.loadTranslations(english, turkish);
+
+    console.log('Home Constructor');
+
+    this.getBackendURL();
+
+    this.inOrg.currentInOrg.subscribe((message) => {
+      this.inOrgCheck = message;
+
+      console.log('inOrgCheck change', this.inOrgCheck);
+      if (this.inOrgCheck) {
+        console.log('in org');
+        this.InOrganization = true;
+      } else {
+        this.InOrganization = false;
+      }
+    });
+
+    // this.directorService.currentIsDirector.subscribe(message => {
+
+    //   this.IsDirector = message;
+
+    // })
+
+    this.env = environment.envName;
+
+    // if not production get the localhost from the environment file
+    if (!environment.production) {
+      this.API = environment.API_URL;
+    }
+    // else it doesn't need to be set
+
+    console.log('this.authService.isExpired()', this.authService.isExpired());
+
+    if (!this.authService.isExpired()) {
+      console.log('currentUser');
+      console.log(localStorage.getItem('currentUser'));
+
+      if (localStorage.getItem('currentUser')) {
+        this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
+
+        console.log(this.currentUser.username);
+        this.userName = this.currentUser.username;
+        this.accessLevel = this.currentUser.accessLevel;
+
+        if (this.accessLevel > 1) {
+          this.IsDirector = true;
+
+          //   this.directorService.changeMessage(this.IsDirector)
+        } else {
+          this.IsDirector = false;
+
+          //   this.directorService.changeMessage(this.IsDirector)
+          //
+        }
+
+        this.getOrganizations();
+      }
+    }
   }
+
+  ngOnInit() {
+    console.log('ngOnInit');
+
+    if (!this.authService.isExpired()) {
+      console.log('currentUser');
+      console.log(localStorage.getItem('currentUser'));
+
+      if (localStorage.getItem('currentUser')) {
+        this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
+
+        console.log(this.currentUser.username);
+        this.userName = this.currentUser.username;
+        this.accessLevel = this.currentUser.accessLevel;
+
+        if (this.accessLevel > 1) {
+          this.IsDirector = true;
+
+          // this.directorService.changeMessage(this.IsDirector)
+        } else {
+          this.IsDirector = false;
+
+          // this.directorService.changeMessage(this.IsDirector)
+        }
+
+
+        this.getOrganizations();
+      }
+    }
+  }// end of ngOnInit
+
+  // check if user is in an organization
+  getOrganizations() {
+    console.log('get organizations');
+
+    this.getUserService.getUserbyUsername(this.userName)
+      .subscribe(
+        (user) => {
+          console.log('user', user);
+
+          if (user.length > 0) {
+            if (user[0].organizations.length > 0) {
+              this.organizations = user[0].organizations;
+
+              console.log('this.organizations', this.organizations);
+
+              this.InOrganization = true;
+
+              this.inOrg.changeMessage(true);
+            } else {
+              console.log('not in any organizations');
+
+              this.InOrganization = false;
+
+              this.inOrg.changeMessage(false);
+            }
+          } else {
+            console.log('no user');
+          }
+        },
+      );
+  }// end of getOrganizations
 
   getBackendURL() {
     if (environment.production) {
