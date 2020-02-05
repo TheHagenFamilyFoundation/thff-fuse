6e19fea3dea09b1b12801323e9ef184643aec599
Logan Born <lcborn4@gmail.com>
2020-02-04 21:59:55 -0500

    about states component

---
+++ src/app/main/home/about-states/about-states.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-about-states',
+  templateUrl: './about-states.component.html',
+  styleUrls: ['./about-states.component.scss']
+})
+export class AboutStatesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
