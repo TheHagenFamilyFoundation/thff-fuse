3b23671c785ab7fffc4dc2d74bf43b39056d7cbb
Logan Born <lcborn4@gmail.com>
2020-02-04 22:46:50 -0500

    state image paths

--- src/app/main/home/about-states/about-states.component.ts
+++ src/app/main/home/about-states/about-states.component.ts
@@ -3,13 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-about-states',
   templateUrl: './about-states.component.html',
-  styleUrls: ['./about-states.component.scss']
+  styleUrls: ['./about-states.component.scss'],
 })
 export class AboutStatesComponent implements OnInit {
+  coloradoImage = './assets/images/colorado.png';
+
+  michiganImage = './assets/images/michigan.png';
+
+  floridaImage = './assets/images/florida.png';
 
   constructor() { }
 
   ngOnInit() {
   }
-
 }
