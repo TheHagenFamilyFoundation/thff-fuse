68a424728d9890256d55f2501558ea9344952112
Logan Born <lcborn4@gmail.com>
2020-02-12 23:15:25 -0500

    grants by year logic

--- src/app/main/grants-awarded/grants-by-year/grants-by-year.component.ts
+++ src/app/main/grants-awarded/grants-by-year/grants-by-year.component.ts
@@ -1,15 +1,59 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { GrantService } from '../../../services/grants/grant.service';
+import { Grant } from '../../../services/grants/grant';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-grants-by-year',
   templateUrl: './grants-by-year.component.html',
-  styleUrls: ['./grants-by-year.component.scss']
+  styleUrls: ['./grants-by-year.component.scss'],
 })
 export class GrantsByYearComponent implements OnInit {
+  title = 'Grants By Year'
 
-  constructor() { }
+  @Input()
+  year: number;
 
-  ngOnInit() {
+  // arrays
+  grants: Grant[];
+
+  constructor(private grantService: GrantService) {
+    console.log('Grants By Year');
   }
 
+  ngOnInit() {
+    this.grants = [];
+
+    console.log(this.year);
+
+
+    this.getGrantsByYear(this.year);
+  }
+
+  getGrantsByYear(year: number): void {
+    this.grantService
+      .getGrantsByYear(year)
+      .subscribe(
+        (grants) => {
+        // debug
+          console.log(grants);
+          console.log('grants');
+          // console.log(this.grants);
+
+          if (this.grants.length == 0) {
+            this.grants = grants;
+          } else {
+            grants.forEach((item, index) => {
+              console.log(item);
+
+              this.grants.push(item);
+            });
+          }
+
+          console.log(this.grants);
+
+        // );
+        },
+      );
+  }// end of getGrantsByYear
 }
