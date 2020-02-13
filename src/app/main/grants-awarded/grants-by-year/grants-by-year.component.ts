ffcba1a06b06e904c0e66b80a567de9c33ddff34
Logan Born <lcborn4@gmail.com>
2020-02-12 22:43:47 -0500

    grants by year component

---
+++ src/app/main/grants-awarded/grants-by-year/grants-by-year.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-grants-by-year',
+  templateUrl: './grants-by-year.component.html',
+  styleUrls: ['./grants-by-year.component.scss']
+})
+export class GrantsByYearComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
