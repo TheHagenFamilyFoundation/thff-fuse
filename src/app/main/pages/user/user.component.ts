23b15ff9ff2fe673a637a787c6dddc63e5fe17ee
Logan Born <lcborn4@gmail.com>
2020-02-16 17:11:20 -0500

    user page component

--- src/app/main/pages/user/user.component.ts
+++ src/app/main/pages/user/user.component.ts
@@ -1,16 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { FuseTranslationLoaderService } from '@fuse/services/translation-loader.service';
 
 import { locale as english } from './i18n/en';
 import { locale as turkish } from './i18n/tr';
 
+import { InOrgService } from '../../../services/user/in-org.service';
+
 @Component({
   selector: 'user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.scss'],
 })
-export class UserComponent {
+export class UserComponent implements OnInit {
+  user;
+
+  inOrgCheck: boolean;
+
   /**
      * Constructor
      *
@@ -18,7 +25,22 @@ export class UserComponent {
      */
   constructor(
         private fuseTranslationLoaderService: FuseTranslationLoaderService,
+        private router: Router, private inOrg: InOrgService,
   ) {
     this.fuseTranslationLoaderService.loadTranslations(english, turkish);
+  }
+
+  ngOnInit() {
+    if (!localStorage.getItem('currentUser')) {
+      // not logged in so redirect to login page
+      this.router.navigate(['/login']);
+    }
+
+    this.user = JSON.parse(localStorage.getItem('currentUser'));
+
+    console.log('User - this.user');
+    console.log(this.user);
+
+    this.inOrg.currentInOrg.subscribe((message) => this.inOrgCheck = message);
   }
 }
