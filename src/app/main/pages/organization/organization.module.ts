9d603a0f45debe8b9efded1e818987ffc8fdf791
Logan Born <lcborn4@gmail.com>
2020-02-22 15:22:45 -0500

    currency and phone mask

--- src/app/main/pages/organization/organization.module.ts
+++ src/app/main/pages/organization/organization.module.ts
@@ -14,21 +14,43 @@ import { CreateOrganizationService } from '../../../services/organization/create
 
 // Components
 import { OrganizationComponent } from './organization.component';
-import { CreateOrganizationComponent } from './create-organization/create-organization.component';
+import { CreateOrganizationComponent } from './create-organization/create-organization.component'; // old
 import { CreateOrganizationFullComponent } from './create-organization-full/create-organization-full.component';
+
+import { NgxCurrencyModule } from 'ngx-currency';
+
+import { PhoneMaskDirective } from '../../../directives/phone-mask.directive';
 
 const routes = [
   {
     path: 'organization',
     component: OrganizationComponent,
   },
+  {
+    path: 'create-organization',
+    component: CreateOrganizationFullComponent,
+  },
 ];
+
+export const customCurrencyMaskConfig = {
+  align: 'right',
+  allowNegative: false,
+  allowZero: true,
+  decimal: '.',
+  precision: 2,
+  prefix: '$',
+  suffix: '',
+  thousands: ',',
+  nullable: false,
+};
 
 @NgModule({
   declarations: [
     OrganizationComponent,
     CreateOrganizationComponent,
     CreateOrganizationFullComponent,
+
+    PhoneMaskDirective,
   ],
   imports: [
     RouterModule.forChild(routes),
@@ -40,12 +62,14 @@ const routes = [
     MatFormFieldModule,
     MatProgressSpinnerModule,
     MatInputModule,
+
+    NgxCurrencyModule.forRoot(customCurrencyMaskConfig),
   ],
   providers: [
     CreateOrganizationService, GetOrganizationService,
   ],
   exports: [
-    OrganizationComponent,
+    OrganizationComponent, PhoneMaskDirective,
   ],
 })
 
