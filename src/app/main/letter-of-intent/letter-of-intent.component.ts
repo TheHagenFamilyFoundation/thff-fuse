c4853a1be6f09d51a40b2eecbc48eb6bc8b3d6b6
Logan Born <lcborn4@gmail.com>
2020-02-21 23:36:25 -0500

    letter of intent component module

---
+++ src/app/main/letter-of-intent/letter-of-intent.component.ts
@@ -0,0 +1,61 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { GetLoiService } from '../../services/loi/get-loi.service';
+import { LOIStatusService } from '../../services/loi/loi-status.service';
+
+@Component({
+  selector: 'app-letter-of-intent',
+  templateUrl: './letter-of-intent.component.html',
+  styleUrls: ['./letter-of-intent.component.scss'],
+})
+export class LetterOfIntentComponent implements OnInit {
+  loiID: any;
+
+  loi: any; // the loi object
+
+  // check basic row height
+  basicRowHeight = 450;
+
+  status: number;
+
+  constructor(
+    private route: ActivatedRoute,
+    public getLoiService: GetLoiService,
+    private loiStatus: LOIStatusService,
+  ) {
+    this.route.params.subscribe((params) => {
+      console.log(params);
+      this.loiID = params.id;
+    });
+  }
+
+  ngOnInit() {
+    console.log('loiID', this.loiID);
+
+    this.getLOI(this.loiID);
+
+    this.loiStatus.currentStatus.subscribe((status) => this.status = Number(status));
+  }
+
+  getLOI(loiID) {
+    console.log('check loi');
+
+    // query database for that loi
+
+    this.getLoiService.getLOIbyID(loiID)
+      .subscribe(
+        (loi) => {
+          console.log('loi', loi);
+
+          this.loi = loi[0];
+
+          this.setStatus();
+        },
+      );
+  }
+
+  setStatus() {
+    this.status = this.loi.status;
+  }
+}
