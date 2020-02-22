0d3aaffefa7b44ec56598c4cdada88cd8d16e85b
Logan Born <lcborn4@gmail.com>
2020-02-21 20:34:07 -0500

    emails and the mat card modules

--- src/app/main/pages/user/user.module.ts
+++ src/app/main/pages/user/user.module.ts
@@ -2,9 +2,22 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
 
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
 import { FuseSharedModule } from '@fuse/shared.module';
 
+import { ChangeEmailService } from '../../../services/user/change-email.service';
+import { ChangePasswordService } from '../../../services/user/change-password.service';
+
+import { UpdateUserService } from '../../../services/user/update-user.service';
+
 import { UserComponent } from './user.component';
+import { UserUpdateMainComponent } from './user-update-main/user-update-main.component';
+import { UserUpdateComponent } from './user-update-main/user-update/user-update.component';
+import { ChangePasswordComponent } from './user-update-main/change-password/change-password.component';
+import { ChangeEmailComponent } from './user-update-main/change-email/change-email.component';
 
 const routes = [
   {
@@ -16,6 +29,11 @@ const routes = [
 @NgModule({
   declarations: [
     UserComponent,
+    UserUpdateMainComponent,
+    UserUpdateComponent,
+    ChangePasswordComponent,
+    ChangeEmailComponent,
+
   ],
   imports: [
     RouterModule.forChild(routes),
@@ -23,6 +41,18 @@ const routes = [
     TranslateModule,
 
     FuseSharedModule,
+
+    // angular material
+    MatCardModule,
+    MatGridListModule,
+    MatFormFieldModule,
+
+  ],
+  providers: [
+    ChangeEmailService,
+    ChangePasswordService,
+
+    UpdateUserService,
   ],
   exports: [
     UserComponent,
