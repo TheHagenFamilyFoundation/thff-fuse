efcd0f553b03e2d926b74f7ab2c390eedc191512
Logan Born <lcborn4@gmail.com>
2020-02-21 23:38:44 -0500

    selected organization component

---
+++ src/app/main/pages/user/user-organization/selected-organization/selected-organization.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-selected-organization',
+  templateUrl: './selected-organization.component.html',
+  styleUrls: ['./selected-organization.component.scss'],
+})
+export class SelectedOrganizationComponent implements OnInit {
+  orgLink = '/organization/'
+
+  link: string;
+
+  orgID: any;
+
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private router: Router,
+  ) { }
+
+  ngOnInit() {
+    // will log the entire data object
+    console.log('this.data', this.data);
+
+    this.orgID = this.data.orgID;
+
+    this.link = this.orgLink + this.data.orgID;
+
+    console.log('this is the link', this.link);
+  }
+
+  cancel() {
+    console.log('cancel pressed');
+  }
+}
