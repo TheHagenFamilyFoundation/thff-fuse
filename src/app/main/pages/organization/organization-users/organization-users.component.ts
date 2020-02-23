5a214793c8b2c0d66e05d6788c2ff247493d9b75
Logan Born <lcborn4@gmail.com>
2020-02-22 19:23:19 -0500

    created organization users

---
+++ src/app/main/pages/organization/organization-users/organization-users.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-organization-users',
+  templateUrl: './organization-users.component.html',
+  styleUrls: ['./organization-users.component.scss']
+})
+export class OrganizationUsersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
