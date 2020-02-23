e19da9c8dec4c30a4e00771ea17022817885a093
Logan Born <lcborn4@gmail.com>
2020-02-22 19:22:59 -0500

    created organization full proposals

---
+++ src/app/main/pages/organization/organization-full-proposals/organization-full-proposals.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-organization-full-proposals',
+  templateUrl: './organization-full-proposals.component.html',
+  styleUrls: ['./organization-full-proposals.component.scss']
+})
+export class OrganizationFullProposalsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
