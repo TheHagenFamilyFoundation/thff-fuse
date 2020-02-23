068d1619553d0d644cb2c5f02f1ce7a90ca62972
Logan Born <lcborn4@gmail.com>
2020-02-22 20:05:18 -0500

    delete doc501c3 check filled in

---
+++ src/app/main/pages/organization/organization-doc501c3/delete-doc501c3-check/delete-doc501c3-check.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+
+@Component({
+  selector: 'app-delete-doc501c3-check',
+  templateUrl: './delete-doc501c3-check.component.html',
+  styleUrls: ['./delete-doc501c3-check.component.scss'],
+})
+export class DeleteDoc501c3CheckComponent implements OnInit {
+  constructor(public dialogRef: MatDialogRef<DeleteDoc501c3CheckComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any) { }
+
+  ngOnInit() {
+  }
+
+  delete501c3() {
+    console.log('delete 510c3');
+
+    const body = {
+      delete: true,
+    };
+
+    this.dialogRef.close(body);
+  }
+
+  cancel() {
+    console.log('cancel the delete');
+  }
+}
