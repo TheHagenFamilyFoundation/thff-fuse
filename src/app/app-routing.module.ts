8beb1fc97efde23e839d8d6b98ebd259b809baf9
Logan Born <lcborn4@gmail.com>
2020-02-22 20:17:18 -0500

    external url provider

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,10 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule, InjectionToken } from '@angular/core';
+import { RouterModule, Routes, ActivatedRouteSnapshot } from '@angular/router';
+
+// Utility
+import { NotFoundComponent } from './utilities/not-found/not-found.component';
+
+const externalUrlProvider = new InjectionToken('externalUrlRedirectResolver');
 
 const routes: Routes = [
 
@@ -22,15 +27,36 @@ const routes: Routes = [
     path: 'pages',
     loadChildren: './main/pages/pages.module#PagesModule',
   },
+
+  {
+    path: 'externalRedirect',
+    resolve: {
+      url: externalUrlProvider,
+    },
+    // We need a component here because we cannot define the route otherwise
+    component: NotFoundComponent,
+  },
+
+
   {
     path: '**',
     redirectTo: 'home',
   },
+
 
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
+  providers: [
+    {
+      provide: externalUrlProvider,
+      useValue: (route: ActivatedRouteSnapshot) => {
+        const externalUrl = route.paramMap.get('externalUrl');
+        window.open(externalUrl, '_self');
+      },
+    },
+  ],
 })
 export class AppRoutingModule { }
