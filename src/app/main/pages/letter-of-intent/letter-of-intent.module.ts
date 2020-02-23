1f3272d4d958ddac33af16a12f4314e03db093d1
Logan Born <lcborn4@gmail.com>
2020-02-22 19:01:25 -0500

    mat grid, button added, currency

--- src/app/main/pages/letter-of-intent/letter-of-intent.module.ts
+++ src/app/main/pages/letter-of-intent/letter-of-intent.module.ts
@@ -6,11 +6,14 @@ import { FuseSharedModule } from '@fuse/shared.module';
 
 import { MatInputModule } from '@angular/material';
 import { MatCardModule } from '@angular/material/card';
-import { MatGridListModule } from '@angular/material';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatButtonModule } from '@angular/material/button';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatSelectModule } from '@angular/material/select';
 import { MatIconModule } from '@angular/material/icon';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
+import { LOIStatusService } from '../../../services/loi/loi-status.service';
 
 import { LetterOfIntentComponent } from './letter-of-intent.component';
 import { LetterOfIntentInfoComponent } from './letter-of-intent-info/letter-of-intent-info.component';
@@ -20,10 +23,26 @@ import { CreateLetterOfIntentFullComponent } from './create-letter-of-intent-ful
 import { LetterOfIntentSubmitComponent } from './letter-of-intent-submit/letter-of-intent-submit.component';
 import { LetterOfIntentSubmitCheckComponent } from './letter-of-intent-submit-check/letter-of-intent-submit-check.component';
 
+import { NgxCurrencyModule } from 'ngx-currency';
+
+export const customCurrencyMaskConfig = {
+  align: 'right',
+  allowNegative: false,
+  allowZero: true,
+  decimal: '.',
+  precision: 2,
+  prefix: '$',
+  suffix: '',
+  thousands: ',',
+  nullable: false,
+};
+
 const routes = [
+
   {
-    path: 'letter-of-intent',
+    path: 'letter-of-intent/:id',
     component: LetterOfIntentComponent,
+    // canActivate: [AuthGuard],
   },
   {
     path: 'create-letter-of-intent',
@@ -55,10 +74,14 @@ const routes = [
     MatSelectModule,
     MatIconModule,
     MatProgressSpinnerModule,
+    MatButtonModule,
+
+    NgxCurrencyModule.forRoot(customCurrencyMaskConfig),
   ],
   exports: [
     LetterOfIntentComponent,
   ],
+  providers: [LOIStatusService],
 })
 
 export class LetterOfIntentModule {
