a41d0d3c745305442c8ca1fe8e61104b532df4a2
Logan Born <lcborn4@gmail.com>
2020-02-22 20:05:53 -0500

    organization users filled in

--- src/app/main/pages/organization/organization-users/organization-users.component.ts
+++ src/app/main/pages/organization/organization-users/organization-users.component.ts
@@ -1,15 +1,116 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component, ViewChild, OnInit, Input,
+} from '@angular/core';
+import { MatPaginator, MatSort, MatTableDataSource } from '@angular/material';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { AddUsersComponent } from './add-users/add-users.component';
+
+import { AddUserService } from '../../../../services/organization/add-user.service';
 
 @Component({
   selector: 'app-organization-users',
   templateUrl: './organization-users.component.html',
-  styleUrls: ['./organization-users.component.scss']
+  styleUrls: ['./organization-users.component.scss'],
 })
 export class OrganizationUsersComponent implements OnInit {
+  @Input()
+  org: any;
 
-  constructor() { }
+  orgID: any;
 
-  ngOnInit() {
+  users: any;
+
+  addUsersHeight: string;
+
+  addUsersWidth: string;
+
+  selectedUsers: any;
+
+  // displayedColumns = ['id', 'name', 'progress', 'color'];
+  displayedColumns = ['username'];
+
+  dataSource: any;// MatTableDataSource<OrganizationData>;
+
+  @ViewChild(MatPaginator, { static: false }) paginator: MatPaginator;
+
+  @ViewChild(MatSort, { static: false }) sort: MatSort;
+
+  constructor(public dialog: MatDialog,
+    private addUserService: AddUserService) {
+    this.addUsersHeight = '750';// 550
+    this.addUsersWidth = '700';
+
+    this.selectedUsers = [];
   }
 
+  ngOnInit() {
+    console.log('this.org', this.org);
+    console.log('this.org.users', this.org.users);
+    this.users = this.org.users;
+    this.dataSource = new MatTableDataSource(this.users);
+    // this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+
+    this.orgID = this.org.id;
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+
+  onRowClicked(row) {
+    console.log('Row clicked: ', row);
+
+    // this.openSelectedOrgDialog(row); //pass in the org from row object
+  }
+
+  addUser() {
+    console.log('add Users');
+
+    // modal
+    this.openAddUsersDialog();
+  }
+
+  openAddUsersDialog(): void {
+    console.log('this.addUsersWidth', this.addUsersWidth);
+
+    const dialogRef = this.dialog.open(AddUsersComponent, {
+      // width: '700px',
+      width: `${this.addUsersWidth}px`,
+      height: `${this.addUsersHeight}px`,
+      data: { org: this.org },
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log('The dialog was closed'); // debug
+
+      console.log('result', result); // debug
+
+      if (result) {
+        this.selectedUsers = result;
+
+        const body = {
+          users: this.selectedUsers,
+          org: this.org,
+        };
+
+        // call service to add users to organization
+        this.addUserService.addUser(body)
+          .subscribe((users) => {
+            console.log('users', users);
+
+            this.getUsers();
+          });
+      }
+    });
+  }
+
+  getUsers() {
+    // pulls list of users that organization has
+    console.log('getting users');
+  }
+
+  onPaginateChange(event) {
+    console.log(JSON.stringify(`Current page index: ${event.pageIndex}`));
+  }
 }
