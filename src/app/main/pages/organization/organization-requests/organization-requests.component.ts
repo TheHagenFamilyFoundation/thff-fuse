f8971be394a36e112d758da4960b2fcc7694fd73
Logan Born <lcborn4@gmail.com>
2020-02-22 19:23:12 -0500

    created organization requests

---
+++ src/app/main/pages/organization/organization-requests/organization-requests.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-organization-requests',
+  templateUrl: './organization-requests.component.html',
+  styleUrls: ['./organization-requests.component.scss']
+})
+export class OrganizationRequestsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
