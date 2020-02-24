31fff0c3420cdd14ed5b662f42824bd4c76b7956
Logan Born <lcborn4@gmail.com>
2020-02-23 23:56:47 -0500

    carousel image as the carousel

--- src/app/main/home/carousel/carousel-image/carousel-image.component.ts
+++ src/app/main/home/carousel/carousel-image/carousel-image.component.ts
@@ -1,16 +1,28 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-carousel-image',
   templateUrl: './carousel-image.component.html',
   styleUrls: ['./carousel-image.component.scss'],
 })
-export class CarouselImageComponent implements OnInit {
-  @Input()
-  img: string;
+export class CarouselImageComponent {
+  @Input() slides;
 
-  constructor() { }
+  constructor() {
+    console.log('slides', this.slides);
+  }
 
-  ngOnInit(): void {
+  currentSlide = 0;
+
+  onPreviousClick() {
+    const previous = this.currentSlide - 1;
+    this.currentSlide = previous < 0 ? this.slides.length - 1 : previous;
+    console.log('previous clicked, new current slide is: ', this.currentSlide);
+  }
+
+  onNextClick() {
+    const next = this.currentSlide + 1;
+    this.currentSlide = next === this.slides.length ? 0 : next;
+    console.log('next clicked, new current slide is: ', this.currentSlide);
   }
 }
