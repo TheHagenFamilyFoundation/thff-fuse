f9c13f4f78c4f66a2eaa995e9881cc8354d413c1
Logan Born <lcborn4@gmail.com>
2020-02-24 22:23:21 -0500

    added mat dialog to organization

--- src/app/main/pages/organization/organization.module.ts
+++ src/app/main/pages/organization/organization.module.ts
@@ -13,6 +13,7 @@ import { MatCardModule } from '@angular/material/card';
 import { MatTableModule } from '@angular/material/table';
 import { MatIconModule } from '@angular/material/icon';
 import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatDialogModule } from '@angular/material/dialog';
 
 // Organization
 import { GetOrganizationService } from '../../../services/organization/get-organization.service';
@@ -103,6 +104,7 @@ export const customCurrencyMaskConfig = {
     MatIconModule,
     MatPaginatorModule,
     MatButtonModule,
+    MatDialogModule,
 
     NgxCurrencyModule.forRoot(customCurrencyMaskConfig),
   ],
