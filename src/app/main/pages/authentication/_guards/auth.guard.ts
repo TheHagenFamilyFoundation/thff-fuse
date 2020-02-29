be3480aa7ea15532b2be893a9ed7bef2fcba5385
Logan Born <lcborn4@gmail.com>
2020-02-29 14:23:56 -0500

    added the auth guard

---
+++ src/app/main/pages/authentication/_guards/auth.guard.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import {
+  Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot,
+} from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Injectable({ providedIn: 'root' })
+export class AuthGuard implements CanActivate {
+  constructor(private router: Router, public snackBar: MatSnackBar) {
+    console.log('help - auth guard');
+  }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    console.log('this is the canActivate function');
+
+    if (localStorage.getItem('currentUser')) {
+      // logged in so return true
+      return true;
+    }
+
+    const snackBarRef = this.snackBar.open('You need to be logged In to access that page', 'OK', {
+      duration: 3000,
+    });
+
+    // not logged in so redirect to login page
+    this.router.navigate(['/login'], { queryParams: { returnUrl: state.url } });
+    return false;
+  }
+}
