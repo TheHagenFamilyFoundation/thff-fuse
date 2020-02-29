9b99d84532273d057f74ee83bd2ca73ed151d9f4
Logan Born <lcborn4@gmail.com>
2020-02-29 14:41:13 -0500

    director module

---
+++ src/app/main/pages/director/director.module.ts
@@ -0,0 +1,69 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+
+import { FuseSharedModule } from '@fuse/shared.module';
+import { DirectorFpsComponent } from './director-fps/director-fps.component';
+import { DirectorLoisComponent } from './director-lois/director-lois.component';
+import { DirectorOrganizationsComponent } from './director-organizations/director-organizations.component';
+import { DirectorMenuComponent } from './director-menu/director-menu.component';
+
+const routes = [
+  // {
+  //   path: 'user',
+  //   component: UserComponent,
+  // },
+];
+
+@NgModule({
+  declarations: [
+
+    DirectorFpsComponent,
+
+    DirectorLoisComponent,
+
+    DirectorOrganizationsComponent,
+
+    DirectorMenuComponent],
+  imports: [
+    RouterModule.forChild(routes),
+
+    TranslateModule,
+
+    FuseSharedModule,
+
+    // angular material
+    MatCardModule,
+    MatGridListModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatProgressSpinnerModule,
+    MatButtonModule,
+    MatDialogModule,
+
+  ],
+  providers: [
+  ],
+  exports: [
+  ],
+  entryComponents: [
+
+  ],
+})
+
+export class DirectorModule {
+}
