6684ad2e319146be9d5089217e086377862f9e0f
Logan Born <lcborn4@gmail.com>
2020-02-29 14:41:24 -0500

    created director-fps

---
+++ src/app/main/pages/director/director-fps/director-fps.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-director-fps',
+  templateUrl: './director-fps.component.html',
+  styleUrls: ['./director-fps.component.scss']
+})
+export class DirectorFpsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
