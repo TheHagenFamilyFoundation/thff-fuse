9d915a7a6c85f0eda456dcca8712e193cf5f5348
Logan Born <lcborn4@gmail.com>
2020-02-29 14:41:43 -0500

    created director-organizations

---
+++ src/app/main/pages/director/director-organizations/director-organizations.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-director-organizations',
+  templateUrl: './director-organizations.component.html',
+  styleUrls: ['./director-organizations.component.scss']
+})
+export class DirectorOrganizationsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
