6c50a56d5a4bd2feb3ec66377703bdb00b89813e
Logan Born <lcborn4@gmail.com>
2020-02-29 20:06:14 -0500

    director selected fp component

---
+++ src/app/main/pages/director/director-fps/director-table-full-proposals/director-selected-fp/director-selected-fp.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-director-selected-fp',
+  templateUrl: './director-selected-fp.component.html',
+  styleUrls: ['./director-selected-fp.component.scss'],
+})
+export class DirectorSelectedFPComponent implements OnInit {
+  fpLink = '/director-fp/'
+
+  link: string;
+
+  fp: any;
+
+  fpID: any;
+
+  HasFP: boolean;
+
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private router: Router,
+  ) {
+    this.HasFP = false;
+  }
+
+  ngOnInit() {
+    // will log the entire data object
+    console.log('this.data', this.data);
+    this.fp = this.data.fp;
+    console.log('this.fp', this.fp);
+    this.HasFP = true;
+    this.fpID = this.data.fpID;
+
+    this.link = this.fpLink + this.fpID;
+
+    console.log('this is the link', this.link);
+  }
+
+  cancel() {
+    console.log('cancel pressed');
+  }
+}
