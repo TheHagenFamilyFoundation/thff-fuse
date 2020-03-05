44eacdb9d5aa973fdeb38a114984f76acf07d068
Logan Born <lcborn4@gmail.com>
2020-03-04 21:10:26 -0500

    added mat button

--- src/app/main/pages/full-proposal/full-proposal.module.ts
+++ src/app/main/pages/full-proposal/full-proposal.module.ts
@@ -8,6 +8,7 @@ import { AuthGuard } from '../../../auth/_guards/auth.guard';
 
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 import { MatSelectModule } from '@angular/material/select';
 import { MatTableModule } from '@angular/material/table';
 import { MatIconModule } from '@angular/material/icon';
@@ -64,6 +65,7 @@ const routes = [
     MatTableModule,
     MatIconModule,
     MatInputModule,
+    MatButtonModule,
   ],
   exports: [
     FullProposalComponent, FullProposalItemsTwoComponent,
