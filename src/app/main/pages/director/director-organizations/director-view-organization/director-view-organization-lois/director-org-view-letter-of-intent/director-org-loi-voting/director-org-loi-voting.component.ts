05d3023c1075a78fc035357a4af5c61adb8b1e3f
Logan Born <lcborn4@gmail.com>
2020-03-07 14:42:11 -0500

    director org loi voting

---
+++ src/app/main/pages/director/director-organizations/director-view-organization/director-view-organization-lois/director-org-view-letter-of-intent/director-org-loi-voting/director-org-loi-voting.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-director-org-loi-voting',
+  templateUrl: './director-org-loi-voting.component.html',
+  styleUrls: ['./director-org-loi-voting.component.scss']
+})
+export class DirectorOrgLoiVotingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
