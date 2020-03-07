7e81e70b0e16a6ac11150899284a6e90bfaeca40
Logan Born <lcborn4@gmail.com>
2020-03-07 14:03:54 -0500

    director view organization filled in

--- src/app/main/pages/director/director-organizations/director-view-organization/director-view-organization.component.ts
+++ src/app/main/pages/director/director-organizations/director-view-organization/director-view-organization.component.ts
@@ -1,15 +1,54 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { GetOrganizationService } from '../../../../../services/organization/get-organization.service';
 
 @Component({
   selector: 'app-director-view-organization',
   templateUrl: './director-view-organization.component.html',
-  styleUrls: ['./director-view-organization.component.scss']
+  styleUrls: ['./director-view-organization.component.scss'],
 })
 export class DirectorViewOrganizationComponent implements OnInit {
+  org: any;
 
-  constructor() { }
+  orgName: string;
 
-  ngOnInit(): void {
+  orgID: string;
+
+  organizationID: string;
+
+  // check basic row height
+  basicRowHeight = 500;
+
+  constructor(private route: ActivatedRoute,
+    private router: Router,
+    public getOrgService: GetOrganizationService) {
+    this.route.params.subscribe((params) => {
+      console.log(params);
+      this.orgID = params.id;
+    });
   }
 
+  ngOnInit() {
+    console.log('orgID', this.orgID);
+
+    this.getOrganization(this.orgID);
+  }
+
+  getOrganization(orgID) {
+    console.log('check organizations');
+
+    // query database for that organization
+    this.getOrgService.getOrgbyID(orgID)
+      .subscribe(
+        (org) => {
+          console.log('org', org);
+
+          this.org = org[0];
+          this.orgName = this.org.name;
+
+          this.organizationID = this.org.id;
+        },
+      );
+  }
 }
