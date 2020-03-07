0653db98d83473d0d675f8c7998ccc6c0e1cd75a
Logan Born <lcborn4@gmail.com>
2020-03-07 14:14:21 -0500

    linter on organization 501c3

--- src/app/services/organization/501c3/get-501c3.service.ts
+++ src/app/services/organization/501c3/get-501c3.service.ts
@@ -6,43 +6,35 @@ import { environment } from '../../../../environments/environment';
 import { AuthService } from '../../../auth/auth.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class Get501c3Service {
-
   API_URL: string;
 
-  constructor(private http: HttpClient, private authService: AuthService, ) {
-
+  constructor(private http: HttpClient, private authService: AuthService) {
     if (!environment.production) {
       this.API_URL = environment.API_URL;
-    }
-    else {
+    } else {
       this.API_URL = this.authService.getBackendURL();
-      console.log('this.API_URL', this.API_URL)
+      console.log('this.API_URL', this.API_URL);
     }
 
-    console.log('this.API_URL', this.API_URL)
-
+    console.log('this.API_URL', this.API_URL);
   }
 
   get501c3(orgID: string): Observable<any> {
+    const urlString = `${this.API_URL}/get501c3/${orgID}`;
 
-
-    let urlString = this.API_URL + "/get501c3/" + orgID;
-
-    console.log('urlString', urlString)
+    console.log('urlString', urlString);
 
     return this.http.get(urlString);
   }
 
   get501c3Info(orgID: string): Observable<any> {
+    const urlString = `${this.API_URL}/org501c3?orgID=${orgID}`;
 
-    let urlString = this.API_URL + "/org501c3?orgID=" + orgID;
-
-    console.log('urlString', urlString)
+    console.log('urlString', urlString);
 
     return this.http.get(urlString);
   }
-
 }
